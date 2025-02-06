387b313158b19c9aabba1719677863dc3399cefa
juniorxzx <juniorrodriguesdev@outlook.com>
2025-02-06 14:04:02 -0300

    instalação do react-icons e ajuste nos @type

--- src/@types/Weather.d.ts
+++ src/@types/Weather.d.ts
@@ -22,4 +22,43 @@ namespace Weather {
     id: number;
     main: string;
   };
+
+  export type daily = {
+    dt: number;
+    sunrise: number;
+    sunset: number;
+    moonrise: number;
+    moonset: number;
+    moon_phase: number;
+    summary: string;
+    temp: Temperature;
+    feels_like: FeelsLike;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    weather: WeatherDetails[];
+    clouds: number;
+    pop: number;
+    snow: number;
+    uvi: number;
+  };
+
+  export type Temperature = {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+
+  export type FeelsLike = {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
 }
